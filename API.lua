0b5accc86530330c050761f12ab246f77f0a9086
crossliner <crosslinerdev@gmail.com>
2021-12-20 10:43:53 +0200

    feat(repo): ready for testing

--- API.lua
+++ API.lua
@@ -1,5 +1,5 @@
 -- api wrapper
-local PORT = 1984; 
+local PORT = 1984; --// change if needed
 
 local request = syn.request or http.request or http_request or request;
 assert(request, "Unsupported");
@@ -7,13 +7,19 @@ assert(request, "Unsupported");
 local function compile(source) 
   local res = request({
     Method = "POST",
+    Url = ("http://localhost:%d/compile"):format(PORT),
     Body = source,
-  })
+    Headers = {
+      ["Content-Type"] = "text/plain"
+    }
+  });
+
+  return res.Body;
 end;
 
 getgenv().moon = {
   execute = function(source) 
-    return self.load(source)(); -- doesnt exactly execute it
+    return self.load(source)(); --// doesnt exactly execute it
   end;
 
   load = function(str) 
@@ -21,6 +27,8 @@ getgenv().moon = {
   end;
 
   compile = compile;
-};
 
--- made by niggers 2021
+  loadfile = function(file)
+    return self.load(readfile(file));
+  end;
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const PORT = 1984;
+
 const moonscript = require("./moonscript");
+const express = require("express");
+const app = express();
+
+app.use(express.text());
+
+app.post("/compile", (req, res) => {
+  res.end(moonscript.compile(req.body));
+});
+
+app.listen(PORT);
